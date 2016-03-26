3ede445bb276fd7f73c7de01cbd454c66df2e53f
RingoKam <ringo_am@hotmail.com>
2016-03-26 18:24:17 -0400

    started the project!

---
+++ ProgrammerStudyAid/ProgrammerStudyAid/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProgrammerStudyAid.Startup))]
+namespace ProgrammerStudyAid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
